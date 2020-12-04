538ea0f35cf535becc34980683e5c54d813354e8
Diego Almeida <diego.almeida@outlook.com>
2020-12-04 13:09:10 -0300

    Adicionada imagem de backdrop

--- react-movies-app/src/components/MovieDetail.js
+++ react-movies-app/src/components/MovieDetail.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MoviesService } from "../api/MoviesService";
 
+import "../styles.css";
+
 export const MovieDetail = (props) => {
     const [movie, setMovie] = useState({ data: [] });
 
@@ -15,18 +17,29 @@ export const MovieDetail = (props) => {
     });
 
     return (
-    <>
-        <div class="jumbotron">
-            <h1 class="display-4">{movie.data.title}</h1>
-            <p class="lead">{movie.data.overview}</p>
-            <hr class="my-4" />
-            <p>Average: {movie.data.vote_average}</p>
-            <p class="lead">
-                <Link to="/" class="btn btn-primary btn-lg" role="button">Back to Home</Link>
-            </p>
-        </div>
-    </>
-    );
+        <>
+            <br />
+            <div className="jumbotron" style={{backgroundImage: `url('http://image.tmdb.org/t/p/w1920_and_h800_multi_faces${movie.data.backdrop_path}')`}}>
 
-    
+                <div className="row" style={{ paddingTop: '10px' }}>
+
+                    <div className="col-sm-2" style={{ paddingBottom: '10px' }}>
+                        <div className="card">
+                            <img className="card-img-top" src={`https://image.tmdb.org/t/p/w220_and_h330_face${movie.data.poster_path}`} alt={movie.data.title} />
+                        </div>
+                    </div>
+
+                    <div className="col-sm-10" style={{ paddingBottom: '10px' }}>
+                        <h1 className="display-4" style={{ color: '#ffffff' }}>{movie.data.title}</h1>
+                        <p className="lead" style={{ color: '#ffffff' }}>{movie.data.overview}</p>
+                        <hr className="my-4" />
+                        <p style={{ color: '#ffffff' }}>Average: {movie.data.vote_average}</p>
+                        <p className="lead">
+                            <Link to="/" className="btn btn-primary btn-lg" role="button">Back to Home</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
--- react-movies-app/src/components/PopularMoviesList.js
+++ react-movies-app/src/components/PopularMoviesList.js
@@ -20,7 +20,7 @@ export const PopularMoviesList = () => {
       {movies.data.results.map((m) => (
         <div className="col-sm-2" style={{ paddingBottom: '10px'}}>
           <div className="card">
-            <img className="card-img-top" src={ 'https://image.tmdb.org/t/p/w220_and_h330_face/' + m.poster_path} alt={m.title} />
+            <img className="card-img-top" src={ 'https://image.tmdb.org/t/p/w220_and_h330_face' + m.poster_path} alt={m.title} />
               <div className="card-body">
                 <h5 className="card-title">{m.title}</h5>
                 <p className="card-text">{m.release_date}</p>
